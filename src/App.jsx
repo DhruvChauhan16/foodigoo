017dcb7e0bd648c5dbfb8a0ff5b83d5955c357a8
DhruvChauhan16 <dc563301@gmail.com>
2025-07-11 13:31:05 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
+import PrivateRoute from './components/PrivateRoute'
 import {ToastContainer} from 'react-toastify'
+import Login from './pages/Login'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <Home />
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
       <ToastContainer />
-    </div>
+    </Router>
   )
 }
 
